f74181c9d0b5c551eada575b50dd211a78f1bccc
Masooma9 <rezaiemasooma3@gmail.com>
2025-12-07 09:25:02 +0430

    week-2 branch

--- week-1/question-1.test.js
+++
@@ -1,8 +0,0 @@
-const add = require('./question-1');
-test('adds 1 + 2 to equal 3', () => {
-    expect(add(1,2)).toBe(3);
-});
-
-test('must not add "He" + "llo" to equal "Hello"', () => {
-    expect(add("He", "llo")).not.toBe("Hello");
-});
--- week-1/question-3.test.js
+++
@@ -1,8 +0,0 @@
-const findLargest = require('./question-3');
-test('finds largest number in [12,20,15,17,11] that is 20', () => {
-    expect(findLargest([12,20,15,17,11])).toBe(20);
-});
-
-test('does not find largest number in "wer234fr" and return an error', () => {
-    expect(findLargest([12,20,15,17,11])).toBe(20);
-});
